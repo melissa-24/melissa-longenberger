7a6f8be58f9162a1a29a72fdb3a59f46dafc85d6
Melissa Longenberger <melissalongenberger24@gmail.com>
2020-11-02 09:03:15 -0500

    pushing changes....projects rendering

--- portfolio/src/components/PastProjects.js
+++ portfolio/src/components/PastProjects.js
@@ -1,11 +1,27 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import axios from 'axios'
 
 const PastProjects = () => {
 
+    const [data, setData] = useState([])
+
+    useEffect(() => {
+        axios
+        .get('https://ml-fullstack-portfolio.herokuapp.com/past-projects')
+        .then((res) => setData(res.data[0]))
+        // .then((res) => console.log(res.data[0]))
+        .catch((err) => console.log(err))
+    }, [])
+
     return (
-        <>
-        Past Projects
-        </>
+        <div className='projects'>
+            <h3>Past Projects</h3>
+            <div className='project-card'>
+                <h3>{data.pastName}</h3>
+                <p>{data.pastDescription}</p>
+                <img src={data.pastUrl} alt='Project img' />
+            </div>
+        </div>
     )
 }
 
--- portfolio/src/components/Projects.js
+++ portfolio/src/components/Projects.js
@@ -1,10 +1,27 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import axios from 'axios'
+
 
 const Projects = () => {
+
+    const [data, setData] = useState([])
+
+    useEffect(() => {
+        axios
+        .get('https://ml-fullstack-portfolio.herokuapp.com/projects')
+        .then((res) => setData(res.data[0]))
+        // .then((res) => console.log(res.data[0]))
+        .catch((err) => console.log(err))
+    }, [])
 
     return (
         <div className='projects'>
             <h3>Current Projects</h3>
+            <div className='project-card'>
+                <h3>{data.name}</h3>
+                <p>{data.description}</p>
+                <img src={data.url} alt='Project img' />
+            </div>
         </div>
     )
 }
